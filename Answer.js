25b5d8ed28902cc110e93d51517289eea478f0dd
md arbaaz <arba689@gmail.com>
2024-03-07 18:11:16 +0530

    changes

--- Answer.js
+++ Answer.js
@@ -1,53 +1,55 @@
 const https = require("https");
 const fs = require("fs");
 
-const result = []
+const resultarray = []
 
 const url = "https://time.com/";
 
 https.get(url, (res) => {
   let body = '';
 
-  res.on('data', (chunk) => {
-    body += chunk;
+  res.on('data', (datarecieving) => {
+    body += datarecieving;
   });
 
   res.on('end', () => {
-    fs.writeFile("Time.html", body, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      processData(body);
-    });
+    processHtmlData(body);
+    // fs.writeFile("Time.html", body, (err) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+      
+    // });
   });
 }).on('error', (err) => {
   console.error("Error making HTTP request:", err);
 });
 
-function processData(htmlContent) {
+function processHtmlData(htmlContent) {
  
-  const data_required = htmlContent.split('<li class="latest-stories__item">');
+  const data_required = htmlContent.split('<li class="latest-stories__item">'); // Here i am Extracting the top 6 latest stories 
   // console.log(data_required);
   console.log("These are the latest 6 stories from times.com");
 
   for (let i = 1; i <= 6; i++) {
-    const link_part = data_required[i];
-    const data_part = link_part.split('"');
-    const link = "https://time.com" + data_part[1];
-    const title = data_part[4].split('>')[1].split("</h3")[0];
-    result.push({title,link});
+    const answer_part = data_required[i]; 
+    const data_part = answer_part.split('"');
+    const link = "https://time.com" + data_part[1];// Here i am extracting the link from the each latest story
+    const title = data_part[4].split('>')[1].split("</h3")[0]; //  Here i am extracting the title from the each latest story
+    resultarray.push({title,link});
     console.log("        ");
     console.log('Title : ' + title);
     console.log('Link : ' + link);
   }
 }
 
+// Creating a server 
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-res.write(JSON.stringify(result));
+res.write(JSON.stringify(resultarray));
 res.end();
 }).listen(3000, function(){
  console.log("server listening at port 3000"); //the server object listens on port 3000
