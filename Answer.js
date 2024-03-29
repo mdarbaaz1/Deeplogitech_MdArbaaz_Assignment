b860ae23dd7b3ce7ff19b7e56db7b24ed3a62a03
md arbaaz <arba689@gmail.com>
2024-03-29 14:49:12 +0530

    added escapeBacklash function

--- Answer.js
+++ Answer.js
@@ -18,19 +18,25 @@ https.get(url, (res) => {
   console.error("Error making HTTP request:", err);
 });
 
+function escapeBackslashes(input) {
+  return input.replace(/\\(.)/g, '\\');
+}
+// this Function will escape the Backlashes before any character for example if the input is "http://ab\\\cdef.com"; the output after running the function is http://ab\cdef.com
+
 function processHtmlData(htmlContent) {
- 
-  const data_required = htmlContent.split('<li class="latest-stories__item">'); 
+
+  const data_required = htmlContent.split('<li class="latest-stories__item">');
   console.log("These are the latest 6 stories from times.com");
 
   for (let i = 1; i <= 6; i++) {
-    const answer_part = data_required[i]; 
-    const data_part = answer_part.split(/(?<!\\)"/); 
-    const link = "https://time.com" + data_part[1].replace(/\\/g, ''); // Extract link without splitting if there's a backslash before "
-    const title = data_part[4].split(/(?<!\\)>/)[1].replace(/\\/g, '').split(/(?<!\\)<\/h3/)[0].replace(/\\/g, ''); // Extract title without splitting if there's a backslash before ">"
+    const answer_part = data_required[i];
+    const data_part = answer_part.split(/(?<!\\)"/);
+    const link = "https://time.com" + data_part[1];// Extract link 
+    const title = data_part[4].split(/(?<!\\)>/)[1].split(/(?<!\\)<\/h3/)[0]; // Extract title 
+    escapeBackslashes(link);
+    escapeBackslashes(title);
+    resultarray.push({ title, link });
 
-    resultarray.push({title, link});
-    
     console.log('\n');
     console.log('Title : ' + title);
     console.log('Link : ' + link);
@@ -43,10 +49,10 @@ const http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   const url = req.url;
- if(url ==='/getTimeStories'){
-    res.write(JSON.stringify(resultarray)); 
+  if (url === '/getTimeStories') {
+    res.write(JSON.stringify(resultarray));
     res.end();
- }
-}).listen(8000, function(){
- console.log("server listening at port 8000");
+  }
+}).listen(8000, function () {
+  console.log("server listening at port 8000");
 });
